38860844679802a63276e624d02ff9aa04b7c8c1
abishekxm <abishekxm6@gmail.com>
2026-06-30 18:22:44 +0530

    Fix Vercel deployment

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ai mentor.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "ai_mentor.settings"
+)
 
 from django.core.wsgi import get_wsgi_application
 
